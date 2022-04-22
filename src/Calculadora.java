bd7f5e961e853b0701ae3f5c7e45385c3c8a9446
Jose Antonio Santiago <joselondres@hotmail.com>
2022-04-22 13:37:10 +0200

    Version 1

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,12 +4,14 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 
+		/* El usuario introduce datos*/
 		Scanner sc= new Scanner(System.in);
 		System.out.print("Escribe la primera cifra: ");  
 		int a= sc.nextInt();  
 		System.out.print("Escribe la segunda cifra: ");  
 		int b= sc.nextInt();  
 
+		/* Realizamos las operaciones y asignamos una variable a cada una de ellas */
 		int miSuma = a+b;
 		int miResta = a-b;
 		int miMultiplicacion = a*b;
@@ -18,3 +20,6 @@ public class Calculadora {
 	}
 
 }
+
+
+
